c5e7b73d9fb5918f44f10b455679ca5be1f2f707
Leonardo12-gif <frezza.trafego@gmail.com>
2025-05-14 01:12:12 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/frezza-digital-growth-hub/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
